77e7760a4f95b7e56765f8e92a84f01cd3790034
hermitex <mercytibs@yahoo.com>
2022-06-08 02:11:52 +0300

    commit tree 37698e3078c50b5c6af1bcd207a05b7086ac07ad

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Tiberius';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toLocaleUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.substring(0,1)}!`;
